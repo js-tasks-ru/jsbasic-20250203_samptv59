b620d8debd19dd7ba876d1542cfa449851d05775
Игорь <samptv59@gmail.com>
2025-02-16 00:25:25 +0300

    Задание 5. min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ')
+  let numbers = []
+  
+  for (let item of arr) {
+    let num = parseFloat(item)
+    if (!isNaN(num)) {
+      numbers.push(num)
+    }
+  }
+    
+  let min = Math.min(...numbers)
+  let max = Math.max(...numbers)
+  
+  return { min, max };
 }
