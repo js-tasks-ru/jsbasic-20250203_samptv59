10a240f2b6506f78833367c07d734706d1d61584
Игорь <samptv59@gmail.com>
2025-02-19 22:36:50 +0300

    Задание 1. Список друзей.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  const ul = document.createElement('ul');
+
+  friends.forEach(item => {
+    const li = document.createElement('li')
+    li.textContent = `${item.firstName} ${item.lastName}`
+    ul.appendChild(li)
+  })
+
+  return ul
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,47 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.querySelectorAll('tr');
+  let headerCells = rows[0].querySelectorAll('td, th'); 
+
+  let genderIndex = Array.from(headerCells).findIndex(cell => cell.textContent === 'Gender');
+  if (genderIndex === -1) return; 
+
+  let ageIndex = Array.from(headerCells).findIndex(cell => cell.textContent === 'Age');
+  if (ageIndex === -1) return; 
+
+  rows.forEach(row => {
+    let cells = row.querySelectorAll('td');
+    
+    if (cells.length > genderIndex) { 
+      let genderCell = cells[genderIndex]; 
+
+      if (genderCell.textContent.trim() === 'm') {
+        row.classList.add('male');
+      } else if (genderCell.textContent.trim() === 'f') {
+        row.classList.add('female');
+      }
+    }
+
+    if (cells.length > ageIndex) {
+      let ageCell = cells[ageIndex]; 
+      let ageValue = parseInt(ageCell.textContent, 10); 
+
+      if (!isNaN(ageValue) && ageValue < 18) { 
+        row.style.textDecoration = 'line-through';
+      }
+    }
+
+    let availableItem = row.querySelector('[data-available="true"]');
+    let unavailableItem = row.querySelector('[data-available="false"]');
+
+    if (availableItem) {
+      row.classList.add('available');
+    }
+    if (unavailableItem) {
+      row.classList.add('unavailable');
+    }
+    
+    if (!availableItem && !unavailableItem) {
+      row.setAttribute('hidden', 'true')
+    }
+  });
 }
