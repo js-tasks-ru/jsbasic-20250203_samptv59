e2b7f60f960634ce366aff42504e7f8cce2fb0e3
Igor <samptv59@gmail.com>
2025-03-17 00:23:08 +0300

    Курсовая работа. Часть 8 - Иконка корзины

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -3,8 +3,8 @@ import createElement from '../../assets/lib/create-element.js';
 export default class CartIcon {
   constructor() {
     this.render();
-
     this.addEventListeners();
+    this.initialTopCoord = null; // Начальная координата верхнего края иконки
   }
 
   render() {
@@ -26,7 +26,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +39,53 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (!this.elem.offsetHeight) return; // Если иконка не видима, выходим
+
+    // Если ширина окна ≤ 767px, сбрасываем стили и выходим
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: '',
+      });
+      return;
+    }
+
+    // Если начальная координата не задана, вычисляем её
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    // Если прокрутка страницы больше начальной координаты, фиксируем иконку
+    if (window.pageYOffset > this.initialTopCoord) {
+      // Вычисляем отступ по горизонтали
+      const container = document.querySelector('.container');
+      if (!container) return; // Если контейнер не найден, выходим
+
+      const containerRight = container.getBoundingClientRect().right;
+      const iconWidth = this.elem.offsetWidth;
+      const leftIndent = Math.min(
+        containerRight + 20, // 20px справа от контейнера
+        document.documentElement.clientWidth - iconWidth - 10 // 10px от правого края окна
+      );
+
+      // Применяем стили для фиксированного позиционирования
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1000,
+        left: `${leftIndent}px`,
+      });
+
+    } else {
+      // Если прокрутка меньше начальной координаты, сбрасываем стили
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: '',
+      });
+    }
   }
 }
